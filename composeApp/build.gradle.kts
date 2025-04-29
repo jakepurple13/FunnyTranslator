e75d191a6fb7be8a9327974fa8ed9e26acf86444
jacobrein <jacob.rein@gabb.com>
2025-04-29 13:44:51 -0600

    - change version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "com.gabb.funnytranslator"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
     }
 
     packaging {
